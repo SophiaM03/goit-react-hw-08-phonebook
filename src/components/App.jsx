7cad62ac20e164a720ebddee50162e0fc9dd3a47
Sophia <sophia@MacBook-Air-Sophia.local>
2023-06-02 17:56:08 +0300

    bugfixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
@@ -10,50 +10,49 @@ import { ContactsPage } from '../page/ContactsPage';
 import LoginPage from '../page/LoginPage';
 import RegisterPage from '../page/RegisterPage';
 import { AppBar } from './AppBar/AppBar';
+import { useAuth } from '../components/hooks/useAuth';
 
-const Container = styled.div`
-  width: 1200px;
-  margin: 0 auto;
-`;
+// const Container = styled.div`
+//   width: 1200px;
+//   margin: 0 auto;
+// `;
 
 export function App() {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(operations.refreshUser());
   }, [dispatch]);
 
-  return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<AppBar />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<RegisterPage />}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<LoginPage />}
-              />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-            }
-          />
-        </Route>
-      </Routes>
-    </Container>
+  return isRefreshing ? (
+    <h2>Refreshing user...</h2>
+  ) : (
+    <Routes>
+      <Route path="/" element={<AppBar />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
